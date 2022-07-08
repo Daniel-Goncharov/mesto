233cb580ea4288fd0415719e4491e911c20fefcc
Daniel Goncharov <goncharov10d@gmail.com>
2022-07-08 21:44:06 +0300

    feat: настроить открытие и закрытие popup и сохраниения данных пользователя введенных в форму

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+const openPopup = document.querySelector('.profile__edit-button');
+const closePopup = document.querySelector('.popup__closed-button');
+const submitButton = document.querySelector('.popup__submit-button');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_job');
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form');
+
+function openUserInfo() {
+  popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
+};
+
+function closeUserInfo() {
+  popup.classList.remove('popup_opened');
+};
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  closeUserInfo();
+};
+
+openPopup.addEventListener('click', openUserInfo);
+closePopup.addEventListener('click', closeUserInfo);
+formElement.addEventListener('submit', formSubmitHandler);
